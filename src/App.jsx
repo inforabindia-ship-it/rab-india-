6e3f17b3fe2c30d7d3768147a9e041aeac9cb1fb
inforabindia-ship-it <inforabindia@gmail.com>
2026-04-13 17:13:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ export default function App() {
     src={images[index]} 
     style={{ 
       width: "100%", 
-      height: "500px",
+      height: "600px",
       display: "block"
     }} 
   />
